8ac60ff25ee4ee2b5ec6ac4d2a32c000cff5c684
unknown <pinguh@naver.com>
2021-08-05 17:15:29 +0900

    ...

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,11 @@
+import { authService } from "fBase";
 import React, { useState } from "react";
 
 const Auth = () =>{ 
-  const[email, setEmail] = useState("");
-  const[password, setPassword] = useState("");
-  const [newAccount,setNewAccount] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
   const onChange = (event) => {
        const {
            target:{name,value},
@@ -14,9 +16,29 @@ const Auth = () =>{
             setPassword(value);
         }
   };
-  const onSubmit = (event) => {
+  const onSubmit = async(event) => {
     event.preventDefault();
+    try{
+        let data;
+        if(newAccount){
+            data = await authService.createUserWithEmailAndPassword(
+                email, 
+                password
+            );
+        } else{
+            data = await authService.signInWithEmailAndPassword(
+                email,
+                password
+            );
+        }
+        console.log(data);
+    }catch(error){
+        setError(error.message);
+    }
   };
+
+  const toggleAccount = () => setNewAccount(prev => !prev);
+
   return(
     <div>
         <form onSubmit = {onSubmit}>
@@ -35,7 +57,9 @@ const Auth = () =>{
                 onChange = {onChange}
              />
             <input type="submit" value={newAccount? "Create Account":"Log In"}/>
+            {error}
         </form>
+        <span onClick={toggleAccount}>{newAccount? "Sign in." : "Create Account"}</span>
         <div>
             <button>Continue with Google</button>
             <button>Continue with Github</button>
